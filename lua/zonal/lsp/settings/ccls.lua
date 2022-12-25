1c6e3902a95b730f83e6894d04b46a391c062767
Kalesh Singh <kaleshsingh@google.com>
2022-12-25 00:10:48 -0800

    Add root_dir to ccls config

--- lua/zonal/lsp/settings/ccls.lua
+++ lua/zonal/lsp/settings/ccls.lua
@@ -7,6 +7,7 @@ return {
 			excludeArgs = { "-frounding-math"} ;
 		};
 	},
+	root_dir = function() return vim.fs.dirname(vim.fs.find({ "compile_commands.json", ".git" }, { upward = true })[1]) end,
 }
 
 
