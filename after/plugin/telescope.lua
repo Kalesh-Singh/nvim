982d3562ae67fc9a299c4ebc38e335131b28e66e
Kalesh Singh <kaleshsingh@google.com>
2022-12-30 23:57:49 -0800

    Improve telescope fuzzy find keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,20 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = '[F]ind [O]pened files' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F] [B]uffers' })
+vim.keymap.set('n', '<C-o>', builtin.oldfiles, { desc = 'Search [O]pened files' })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Search [B]uffers' })
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Search [P]aths / Files' })
+vim.keymap.set('n', '<C-h>', builtin.help_tags, { desc = 'Search [H]elp' })
+vim.keymap.set('n', '<C-w>', builtin.grep_string, { desc = 'Search current [W]ord' })
+vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Search by [G]rep' })
+vim.keymap.set('n', '<C-e>', builtin.diagnostics, { desc = 'Search [E]rrors / Diagnostics' })
+
+-- Search Linux Kernel Documentation
+vim.keymap.set('n', '<C-l>',
+  ":Telescope live_grep search_dirs={'Documentation/'} prompt_title=\\ Linux\\ Kernel\\ Documentation\\ <cr>",
+  { desc = 'Search [L]inux Documentation' })
+
+-- TODO: Doesn't work. Maybe it needs FZF?
+-- Anyway's it's a nice to have. Can get by with /
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -9,16 +22,3 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
-
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-
--- Search Linux Kernel Documentation
-vim.keymap.set('n', 'SD',
-  ":Telescope live_grep search_dirs={'Documentation/'} prompt_title=\\ Linux\\ Kernel\\ Documentation\\ <cr>",
-  { desc = '[S]earch [D]ocumentation' })
